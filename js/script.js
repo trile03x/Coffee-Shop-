9b182c411d706499c74d600bbf0a9a448e31772e
trile03x <levantri6839@gmail.com>
2024-03-07 16:41:54 +0700

    header

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 let navbar = document.querySelector('.navbar');
 let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.add('active');
+    navbar.classList.toggle('active');
     cartItem.classList.remove('active');
 }
 document.querySelector('#cart-btn').onclick = () => {
-    cartItem.classList.add('active');
+    cartItem.classList.toggle('active');
     navbar.classList.remove('active');
 }
